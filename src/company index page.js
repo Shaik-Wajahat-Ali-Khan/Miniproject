34fa9e704f5c7cf315e0019382e1b6d494b60f13
Shaik-Wajahat-Ali Khan <aliwajahatkhan345@gmail.com>
2024-09-06 20:42:16 +0530

    completed

--- src/company index page.js
+++ src/company index page.js
@@ -10,24 +10,35 @@ const CompanyIndexPage = () => {
           <h2>Introduction to Salesforce</h2>
           <p>Delve into the platform's capabilities by exploring key features, modules & the benefits of Salesforce for businesses.</p>
           <div className="learn-more-container">
-            <span>Last Date: 2023-08-31</span>
-            </div>
-            <div className='learn-more-container'>
-            <a href="#" className="learn-more">Learn More</a>
-            <button className="apply-btn">Apply</button>
+            <div>Last Date: 2023-08-31</div>
+            <div >
+            
+            <a href="#" className="learn-more">Learn More</a></div>
+            <div><button className="apply-btn">Apply</button></div>
+
           </div>
           </div>
         <div className="category">
           <h2>Build Critical Skills for Job-readiness & Employability</h2>
           <p>Understand the growing demand for Data skills, the essential skills needed, and how to develop proficiency through targeted educational resources & certification programs.</p>
-          <a href="#" className="learn-more">Learn More</a>
+          <div className="learn-more-container">
+          <div>Last Date:3-9-2024</div>
+          <div><a href="#" className="learn-more">Learn More</a></div>
+          
+          <div><button className='apply-btn'>Apply</button></div>
+        </div>
         </div>
         <div className="category">
           <h2>Woke master cleanse drinking vinegar salvia</h2>
           <p>Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-          <a href="#" className="learn-more">Learn More</a>
+          <div className="learn-more-container">
+            <div>Last Date:4-9-2024</div>
+          <div><a href="#" className="learn-more">Learn More</a></div>
+          
+          <div><button className='apply-btn'>Apply</button></div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
